60987a2f71e9dbc2b57981b6d3e0ad21b8512dee
AshwiniMurhade <97503550+AshwiniMurhade@users.noreply.github.com>
2022-04-11 17:43:05 +0530

    Add files via upload

---
+++ SecondArea.java
@@ -0,0 +1,14 @@
+package Second;
+
+public class SecondArea {
+public static void main(String args[]) {
+ float width = 3f;
+ float height = 6f;
+ float area_rec;
+ float area_square;
+  area_rec = width*height;
+  area_square = (height)*(height);
+ System.out.println("the area of width = " +width+ "and height" +height+ " is "+area_rec);
+ System.out.println("the area of square =  " +area_square);
+}
+}
