e6a7b15c17f378df210aa4859a35fc1024167d43
AshwiniMurhade <97503550+AshwiniMurhade@users.noreply.github.com>
2022-04-11 17:43:49 +0530

    Add files via upload

---
+++ SecondNumber.java
@@ -0,0 +1,22 @@
+package Second;
+
+public class SecondNumber {
+public static void main(String args[]) {
+	int num = 23432;
+	int d;
+	d = num % 10;
+	System.out.println(d);
+	num =num / 10;
+	d = num % 10;
+	System.out.println(d);
+	num = num / 10;
+	d= num % 10;
+	System.out.println(d);
+	num = num / 10;
+	d = num % 10;
+	System.out.println(d);
+	num = num / 10;
+	d = num % 10;
+	System.out.println(d); 
+}
+}
