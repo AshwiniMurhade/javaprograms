588d450401bbc0b98c9e6fee9a509b2aa5f2d3d7
AshwiniMurhade <97503550+AshwiniMurhade@users.noreply.github.com>
2022-04-11 17:44:49 +0530

    Add files via upload

---
+++ SecondProject.java
@@ -0,0 +1,16 @@
+package Second;
+
+public class SecondProject {
+public static void main(String args[]) {
+int p=3;
+float s=2.2f;
+char a='a';
+double b=33.33;
+System.out.println("the value of integer is" +p);
+System.out.println("the value of float is" +s);
+System.out.println("the value of charactor is" +a);
+System.out.println("the value of double is" +b);
+
+
+}
+}
