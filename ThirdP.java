52fe674d8160ea06b153a9743485055bde9d7983
AshwiniMurhade <97503550+AshwiniMurhade@users.noreply.github.com>
2022-04-11 17:49:11 +0530

    Add files via upload

---
+++ ThirdP.java
@@ -0,0 +1,13 @@
+package third;
+
+public class ThirdP {
+public static void main (String args[]) {
+	int a,b;
+	 a=2; b=4;
+	boolean c;
+	c = ( a>b);
+	System.out.println(c);
+	c= (a<b);
+	System.out.println(c);
+}
+}
