546dcf1581322b3032241101e35e40a4cf755bd4
AshwiniMurhade <97503550+AshwiniMurhade@users.noreply.github.com>
2022-04-11 23:46:47 +0530

    demohibernaterel

---
+++ Laptop.java
@@ -0,0 +1,27 @@
+package ashu1;
+
+	import javax.persistence.Entity;
+	import javax.persistence.Id;
+	import javax.persistence.Table;
+
+	@Entity
+	public class Laptop {
+		@Id
+		private int lid;
+		private String lname;
+		public int getLid() {
+			return lid;
+		}
+		public void setLid(int lid) {
+			this.lid = lid;
+		}
+		public String getLname() {
+			return lname;
+		}
+		public void setLname(String lname) {
+			this.lname = lname;
+		
+		
+	}
+	
+}
